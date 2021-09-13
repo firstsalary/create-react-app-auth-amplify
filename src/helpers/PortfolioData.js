7aaf49288544a866372beae63c380bcf6fa92703
Balbir Singh <balbir97@gmail.com>
2021-09-12 23:35:57 -0700

    added

--- src/helpers/PortfolioData.js
+++ src/helpers/PortfolioData.js
@@ -2,13 +2,25 @@ import first from '../images/projectsicon/first.jpg'
 import second from '../images/projectsicon/second.jpg'
 import third from '../images/projectsicon/third.jpg'
 import fourth from '../images/projectsicon/fourth.jpg'
+import fifth from '../images/projectsicon/fifth.jpg'
+import six from '../images/projectsicon/six.jpg'
+import seventh from '../images/projectsicon/seventh.jpg'
+import eight from '../images/projectsicon/eight.jpg'
+import nine from '../images/projectsicon/nine.jpg'
+import ten from '../images/projectsicon/ten.jpg'
+import eleven from '../images/projectsicon/eleven.jpg'
+import tweleve from '../images/projectsicon/tweleve.jpg'
+import thirteen from '../images/projectsicon/thirteen.jpg'
+import fourteen from '../images/projectsicon/fourteen.jpg'
+import fifteen from '../images/projectsicon/fifteen.jpg'
+import sixteen from '../images/projectsicon/sixteen.jpg'
 
 export const PortfolioData = [
     {
         image: first,
         link: 'https://www.firstsalary.net',
-        desc: 'first job',
-        summary: 'first job in the corporate life',
+        desc: 'one job',
+        summary: 'one job in the corporate life',
         tech: 'React'
 
     },
@@ -31,9 +43,96 @@ export const PortfolioData = [
     {
         image: fourth,
         link: 'https://www.firstsalary.net',
-        desc: 'third job',
-        summary: 'third job in the corporate life',
+        desc: 'fourth job',
+        summary: 'fourth job in the corporate life',
         tech: 'Angular'
 
     },
+    {
+        image: fifth,
+        link: 'https://www.firstsalary.net',
+        desc: 'fifth job',
+        summary: 'fifth job in the corporate life',
+        tech: 'Angular'
+
+    },
+    {
+        image: six,
+        link: 'https://www.firstsalary.net',
+        desc: 'six job',
+        summary: 'six job in the corporate life',
+        tech: 'Angular'
+
+    },
+    {
+        image: seventh,
+        link: 'https://www.firstsalary.net',
+        desc: 'seventh job',
+        summary: 'seventh job in the corporate life',
+        tech: 'Angular'
+
+    },
+    {
+    image: eight,
+    link: 'https://www.firstsalary.net',
+    desc: 'eight job',
+    summary: 'eight job in the corporate life',
+    tech: 'Angular'
+    },
+    {
+        image: nine,
+        link: 'https://www.firstsalary.net',
+        desc: 'nine job',
+        summary: 'nine job in the corporate life',
+        tech: 'Angular'
+    },
+    {
+        image: ten,
+        link: 'https://www.firstsalary.net',
+        desc: 'ten job',
+        summary: 'ten job in the corporate life',
+        tech: 'Angular'
+    },
+    {
+        image: eleven,
+        link: 'https://www.firstsalary.net',
+        desc: 'eleven job',
+        summary: 'eleven job in the corporate life',
+        tech: 'Angular'
+    },
+    {
+        image: tweleve,
+        link: 'https://www.firstsalary.net',
+        desc: 'tweleve job',
+        summary: 'tweleve job in the corporate life',
+        tech: 'Angular'
+    },
+    {
+        image: thirteen,
+        link: 'https://www.firstsalary.net',
+        desc: 'thirteen job',
+        summary: 'thirteen job in the corporate life',
+        tech: 'Angular'
+    },
+    {
+        image: fourteen,
+        link: 'https://www.firstsalary.net',
+        desc: 'fourteen job',
+        summary: 'fourteen job in the corporate life',
+        tech: 'Angular'
+    },
+    {
+        image: fifteen,
+        link: 'https://www.firstsalary.net',
+        desc: 'fifteen job',
+        summary: 'fifteen job in the corporate life',
+        tech: 'Angular'
+    },
+    {
+        image: sixteen,
+        link: 'https://www.firstsalary.net',
+        desc: 'sixteen job',
+        summary: 'sixteen job in the corporate life',
+        tech: 'Angular'
+    }
 ]
